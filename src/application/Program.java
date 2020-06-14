80cc895d6b275a4b0f7e9491c5f653a0bb6658e9
williangbezerra <williangomesbezerra@gmail.com>
2020-06-13 23:24:26 -0300

    Criado Enum Color e Classes Position, Piece, Board, ChessPiece, ChessMatch e IU

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
 
+       ChessMatch chessMatch = new ChessMatch();
+       UI.printBoard(chessMatch.getPieces());
 	}
 
 }
